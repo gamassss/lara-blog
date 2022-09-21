9f77654e23cf47e9bad20963a0f9c90204edbeed
gamassss <gamassakbar@gmail.com>
2022-09-21 15:51:01 +0700

    adding relationship to post and category table

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article>
         <h1>{{ $post->title }}</h1>
-        <h5 class="mb-4" >{{ $post->author }}</h5>
+        <h5 class="mb-4" >{{ $post->author }}, Kategori <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
         {!! $post->body !!}
     </article>
     <a href="/posts">Back to Posts</a>
